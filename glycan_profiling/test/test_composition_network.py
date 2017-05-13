2adf314279a53b28f388a2ffb6401322e8dd6c0f
Joshua Klein <mobiusklein@gmail.com>
2017-05-12 23:40:36 -0400

    Updated tests for combinatorics and network

--- glycan_profiling/test/test_composition_network.py
+++ glycan_profiling/test/test_composition_network.py
@@ -2,7 +2,12 @@ import unittest
 
 import glypy
 import pickle
+
+from textwrap import dedent
+from io import BytesIO
+
 from glycan_profiling.database import composition_network
+from glycan_profiling.database.builder.glycan import constrained_combinatorics
 
 compositions = [
     glypy.glycan_composition.FrozenGlycanComposition(HexNAc=2, Hex=i) for i in range(3, 10)
@@ -10,6 +15,93 @@ compositions = [
     glypy.glycan_composition.FrozenGlycanComposition(HexNAc=4, Hex=5, NeuAc=2),
     glypy.glycan_composition.FrozenGlycanComposition(HexNAc=3, Hex=5),
 ]
+
+
+class NeighborhoodWalkerTest(unittest.TestCase):
+    def make_human_definition_buffer(self):
+        text = b'''Hex 3 10
+HexNAc 2 9
+Fuc 0 5
+NeuAc 0 5
+
+Fuc < HexNAc
+HexNAc > NeuAc + 1'''
+        buff = BytesIO()
+        buff.write(text)
+        buff.seek(0)
+        return buff
+
+    def make_mammalian_definition_buffer(self):
+        text = b'''Hex 3 10
+HexNAc 2 9
+Fuc 0 5
+NeuAc 0 5
+NeuGc 0 5
+
+Fuc < HexNAc
+HexNAc > (NeuAc + NeuGc) + 1'''
+        buff = BytesIO()
+        buff.write(text)
+        buff.seek(0)
+        return buff        
+
+    def generate_compositions(self, rule_buffer):
+        rules, constraints = constrained_combinatorics.parse_rules_from_file(rule_buffer)
+        compositions = list(constrained_combinatorics.CombinatoricCompositionGenerator(
+            rules_table=rules, constraints=constraints))
+        # strip out glycan classes
+        compositions = [c[0] for c in compositions]
+        return compositions
+
+    def test_human_network(self):
+        rules_buffer = self.make_human_definition_buffer()
+        compositions = self.generate_compositions(rules_buffer)
+        g = composition_network.CompositionGraph(compositions)
+        self.assertEqual(len(g), 1424)
+        walker = composition_network.NeighborhoodWalker(g)
+        
+        neighborhood_sizes = {
+        "tri-antennary": 164,
+        "bi-antennary": 84,
+        "asialo-bi-antennary": 96,
+        "tetra-antennary": 252,
+        "hybrid": 64,
+        "over-extended": 170,
+        "asialo-tri-antennary": 120,
+        "penta-antennary": 336,
+        "asialo-penta-antennary": 144,
+        "high-mannose": 16,
+        "asialo-tetra-antennary": 136}
+        for k, v in neighborhood_sizes.items():
+            self.assertEqual(v, len(walker.neighborhood_maps[k]), "%s had %d members, not %d" % (
+                k, len(walker.neighborhood_maps[k]), v))
+
+    def test_mammalian_network(self):
+        rules_buffer = self.make_mammalian_definition_buffer()
+        compositions = self.generate_compositions(rules_buffer)
+        g = composition_network.CompositionGraph(compositions)
+        self.assertEqual(len(g), 5096)
+
+        walker = composition_network.NeighborhoodWalker(g)
+
+        neighborhood_sizes = {
+        'tri-antennary': 312,
+        'bi-antennary': 120,
+        'asialo-bi-antennary': 144,
+        'tetra-antennary': 600,
+        'hybrid': 92,
+        'over-extended': 363,
+        'asialo-tri-antennary': 180,
+        'penta-antennary': 960,
+        'asialo-penta-antennary': 216,
+        'high-mannose': 16,
+        'asialo-tetra-antennary': 204,
+        }
+        for k, v in neighborhood_sizes.items():
+            self.assertEqual(v, len(walker.neighborhood_maps[k]), "%s had %d members, not %d" % (
+                k, len(walker.neighborhood_maps[k]), v))
+
+
 
 
 class CompositionGraphTest(unittest.TestCase):
@@ -59,6 +151,8 @@ class CompositionGraphTest(unittest.TestCase):
         g = composition_network.CompositionGraph(compositions)
         g.create_edges(1)
         self.assertEqual(g, g.clone())
+
+
 
 
 if __name__ == '__main__':
--- glycan_profiling/test/test_constrained_combinatorics.py
+++ glycan_profiling/test/test_constrained_combinatorics.py
@@ -9,12 +9,11 @@ from glycan_profiling.database.builder.glycan import constrained_combinatorics
 FILE_SOURCE = '''Hex 3 12
 HexNAc 2 10
 Fuc 0 5
-NeuAc 0 4
+Neu5Ac 0 4
 
 Fuc < HexNAc
 HexNAc > NeuAc + 1
-'''
-
+'''      # The mismatch between Neu5Ac <-> NeuAc tests the IUPAC normalization mechanism
 
 class GlycanCombinatoricsTests(unittest.TestCase):
 
@@ -24,7 +23,7 @@ class GlycanCombinatoricsTests(unittest.TestCase):
         return file_name
 
     def clear_file(self, path):
-        open(path, 'wb')
+        open(path, 'wb').close()
 
     def test_run(self):
         file_name = self.setup_tempfile()
@@ -35,8 +34,10 @@ class GlycanCombinatoricsTests(unittest.TestCase):
             constrained_combinatorics.DBGlycanComposition.hypothesis_id == builder.hypothesis_id,
             constrained_combinatorics.DBGlycanComposition.composition == "{Hex:3; HexNAc:2}").one()
         self.assertAlmostEqual(inst.calculated_mass, 910.32777, 3)
+        i = 0
         for composition in builder.query(constrained_combinatorics.DBGlycanComposition):
             composition = composition.convert()
+            i += 1
             self.assertGreater(composition["HexNAc"], composition['Neu5Ac'])
         builder.engine.dispose()
         self.clear_file(file_name + '.db')
