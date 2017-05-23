ebed25de769dd08e10794d6b36dc94cb442657f4
Joshua Klein <mobiusklein@gmail.com>
2017-05-23 10:24:14 -0400

    Added more simple charge scoring models

--- glycan_profiling/scoring/charge_state.py
+++ glycan_profiling/scoring/charge_state.py
@@ -24,6 +24,39 @@ class UniformChargeStateScoringModel(ChargeStateDistributionScoringModelBase):
         return min(0.4 * chromatogram.n_charge_states, 1.0)
 
 
+class DecayRateChargeStateScoringModel(ChargeStateDistributionScoringModelBase):
+    def __init__(self, step=0.4, rate=1.5):
+        self.step = step
+        self.rate = rate
+
+    def score(self, chromatogram, *args, **kwargs):
+        k = chromatogram.n_charge_states
+        return decay(k, self.step, self.rate)
+
+
+class LogarithmicChargeStateScoringModel(ChargeStateDistributionScoringModelBase):
+    def __init__(self, steps=5):
+        self.base = steps
+
+    def _logarithmic_change_of_base(self, k):
+        if k >= self.base:
+            return 1.0
+        return np.log(k) / np.log(self.base)
+
+    def score(self, chromatogram, *args, **kwargs):
+        # Ensure k > 1 so that the value is greater than 0.0
+        # as `log(1) = 0`
+        k = chromatogram.n_charge_states + 1
+        return self._logarithmic_change_of_base(k)
+
+
+def decay(x, step=0.4, rate=1.5):
+    v = 0
+    for i in range(x):
+        v += (step / (i + rate))
+    return v
+
+
 def ones(x):
     return (x - (np.floor(x / 10.) * 10))
 
@@ -45,35 +78,36 @@ def get_sign(num):
         return -1
 
 
+uniform_model = UniformChargeStateScoringModel()
+decay_model = DecayRateChargeStateScoringModel()
+
+
 class MassScalingChargeStateScoringModel(ChargeStateDistributionScoringModelBase):
     def __init__(self, table, neighborhood_width=100.):
         self.table = table
         self.neighborhood_width = neighborhood_width
 
+    def get_neighborhood_key(self, neutral_mass):
+        neighborhood = neighborhood_of(neutral_mass, self.neighborhood_width)
+        return neighborhood
+
     def score(self, chromatogram, *args, **kwargs):
         total = 0.
-        neighborhood = neighborhood_of(chromatogram.neutral_mass, self.neighborhood_width)
+        neighborhood = self.get_neighborhood_key(chromatogram.neutral_mass)
         if neighborhood not in self.table:
             warnings.warn(
                 ("%f was not found for this charge state "
                  "scoring model. Defaulting to uniform model") % neighborhood)
-            return UniformChargeStateScoringModel().score(chromatogram, *args, **kwargs)
+            return uniform_model.score(chromatogram, *args, **kwargs)
         bins = self.table[neighborhood]
 
-        bin_sign = get_sign(tuple(bins)[0])
-        chrom_sign = get_sign(tuple(chromatogram.charge_states)[0])
-        if bin_sign != chrom_sign:
-            chrom_sign = -1
-        else:
-            chrom_sign = 1
-
         for charge in chromatogram.charge_states:
-            charge = chrom_sign * charge
+            charge = abs(charge)
             try:
-                total += bins[charge]
+                total += bins[(charge)]
             except KeyError:
-                warnings.warn("%d not found for this mass range (%f). Using bin average (%r, %r)" % (
-                    charge, neighborhood, bin_sign, chromatogram.charge_states))
+                warnings.warn("%d not found for this mass range (%f). Using bin average (%r)" % (
+                    charge, neighborhood, chromatogram.charge_states))
                 total += sum(bins.values()) / float(len(bins))
         total = min(total, 1.0)
         return total
@@ -108,7 +142,6 @@ class MassScalingChargeStateScoringModel(ChargeStateDistributionScoringModelBase
         return cls(model_table, neighborhood_width)
 
     def save(self, file_obj):
-        import json
         json.dump(
             {"neighborhood_width": self.neighborhood_width, "table": self.table},
             file_obj, indent=4, sort_keys=True)
@@ -120,7 +153,7 @@ class MassScalingChargeStateScoringModel(ChargeStateDistributionScoringModelBase
         width = float(data.pop("neighborhood_width"))
 
         def numeric_keys(table, dtype=float, convert_value=lambda x: x):
-            return {dtype(k): convert_value(v) for k, v in table.items()}
+            return {abs(dtype(k)): convert_value(v) for k, v in table.items()}
 
         table = numeric_keys(table, convert_value=lambda x: numeric_keys(x, int))
 
