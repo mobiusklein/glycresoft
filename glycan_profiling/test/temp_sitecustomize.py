f268e9baf69e82b4427b387d3f6d12e3ad0f3d52
Joshua Klein <mobiusklein@gmail.com>
2017-09-04 22:49:27 -0400

    unused old test harness

--- glycan_profiling/test/temp_sitecustomize.py
+++
@@ -1,8 +0,0 @@
-import os
-try:
-    import coverage
-
-    os.environ['COVERAGE_PROCESS_START'] = os.path.join(os.path.dirname(__file__), ".coveragerc")
-    coverage.process_startup()
-except ImportError:
-    pass
