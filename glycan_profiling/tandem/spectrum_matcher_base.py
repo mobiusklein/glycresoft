dea71f7c6c87df0fdb2e273de43dc377da045351
Joshua Klein <mobiusklein@gmail.com>
2017-09-08 17:48:13 -0400

    refactored scan data structures

--- glycan_profiling/tandem/spectrum_matcher_base.py
+++ glycan_profiling/tandem/spectrum_matcher_base.py
@@ -11,6 +11,9 @@ except ImportError:
 from ms_deisotope import DeconvolutedPeakSet
 
 from glycan_profiling.task import TaskBase
+from glycan_profiling.structure import (
+    ScanStub,
+    ScanWrapperBase)
 from .ref import TargetReference, SpectrumReference
 
 
@@ -36,69 +39,6 @@ def group_by_precursor_mass(scans, window_size=1.5e-5):
         last_scan = scan
     groups.append(current_group)
     return groups
-
-
-class ScanStub(object):
-    """A stub for holding precursor information and
-    giving a Scan-like interface for accessing just that
-    information. Provides a serialized-like interface
-    which clients can use to load the real scan.
-
-    Attributes
-    ----------
-    id : str
-        The scan ID for the proxied scan
-    precursor_information : PrecursorInformation
-        The information describing the relevant
-        metadata for scheduling when and where this
-        scan should be processed, where actual loading
-        will occur.
-    bind : MzMLLoader
-        A resource to use to load scans with by scan id.
-    """
-    def __init__(self, precursor_information, bind):
-        self.id = precursor_information.product_scan_id
-        self.precursor_information = precursor_information
-        self.bind = bind
-
-    @property
-    def scan_id(self):
-        return self.id
-
-    def convert(self, *args, **kwargs):
-        try:
-            return self.bind.get_scan_by_id(self.id)
-        except AttributeError:
-            raise KeyError(self.id)
-
-
-class ScanWrapperBase(object):
-    __slots__ = []
-
-    @property
-    def scan_id(self):
-        self.requires_scan()
-        return self.scan.scan_id
-
-    @property
-    def precursor_ion_mass(self):
-        self.requires_scan()
-        neutral_mass = self.scan.precursor_information.extracted_neutral_mass
-        return neutral_mass
-
-    @property
-    def scan_time(self):
-        self.requires_scan()
-        return self.scan.scan_time
-
-    @property
-    def precursor_information(self):
-        self.requires_scan()
-        return self.scan.precursor_information
-
-    def requires_scan(self):
-        if self.scan is None:
-            raise ValueError("%s is detatched from Scan" % (self.__class__.__name__))
 
 
 class SpectrumMatchBase(ScanWrapperBase):
