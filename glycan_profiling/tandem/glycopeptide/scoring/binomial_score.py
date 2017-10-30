fix numerical errors in signature detection

glycan_profiling/tandem/glycopeptide/scoring/binomial_score.py
@@ -93,19 +93,32 @@ def _intensity_tiers(peak_list, matched_peaks, total_product_ion_count):
     counts = dict()
     last_count = total_product_ion_count
     next_count = (matched_intensities > m1).sum()
-    counts[1] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[1] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[1] = binomial_tail_probability(last_count, next_count, 0.5)
     last_count = next_count
 
     next_count = (matched_intensities > m2).sum()
-    counts[2] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[2] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[2] = binomial_tail_probability(last_count, next_count, 0.5)
     last_count = next_count
 
     next_count = (matched_intensities > m3).sum()
-    counts[3] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[3] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[3] = binomial_tail_probability(last_count, next_count, 0.5)
+
     last_count = next_count
 
     next_count = (matched_intensities > m4).sum()
-    counts[4] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[4] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[4] = binomial_tail_probability(last_count, next_count, 0.5)
     return counts
 
 
@@ -118,19 +131,32 @@ def _score_tiers(peak_list, matched_peaks, total_product_ion_count):
     counts = dict()
     last_count = total_product_ion_count
     next_count = (matched_intensities > m1).sum()
-    counts[1] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[1] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[1] = binomial_tail_probability(last_count, next_count, 0.5)
     last_count = next_count
 
     next_count = (matched_intensities > m2).sum()
-    counts[2] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[2] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[2] = binomial_tail_probability(last_count, next_count, 0.5)
     last_count = next_count
 
     next_count = (matched_intensities > m3).sum()
-    counts[3] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[3] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[3] = binomial_tail_probability(last_count, next_count, 0.5)
+
     last_count = next_count
 
     next_count = (matched_intensities > m4).sum()
-    counts[4] = binomial_tail_probability(last_count, next_count, 0.5)
+    if last_count == next_count:
+        counts[4] = binomial_tail_probability(last_count, next_count - 1, 0.5)
+    else:
+        counts[4] = binomial_tail_probability(last_count, next_count, 0.5)
     return counts
 
 
@@ -140,9 +166,9 @@ def binomial_intensity(peak_list, matched_peaks, total_product_ion_count):
     counts = _intensity_tiers(peak_list, matched_peaks, total_product_ion_count)
 
     prod = 0
-    for v in counts.values():
+    for k, v in counts.items():
         if v == 0:
-            continue
+            v = 1e-20
         prod += np.log(v)
     return np.exp(prod)
 

glycan_profiling/tandem/glycopeptide/scoring/glycan_signature_ions.py
@@ -65,16 +65,24 @@ class GlycanCompositionSignatureMatcher(GlycopeptideSpectrumMatcherBase):
             ratio = peak.intensity / self.maximum_intensity
             if ratio < 0.01:
                 continue
-            component = -10 * np.log10(1 - ratio)
-            if np.isnan(component):
+            x = 1 - ratio
+            if x <= 0:
                 component = 20
+            else:
+                component = -10 * np.log10(x)
+                if np.isnan(component):
+                    component = 20
             penalty += component
         for key, peak in self.expected_matches.items():
             if peak is None:
                 importance = self.estimate_missing_ion_importance(key)
-                component = -10 * np.log10(1 - importance)
-                if np.isnan(component):
+                x = 1 - importance
+                if x <= 0:
                     component = 20
+                else:
+                    component = -10 * np.log10(x)
+                    if np.isnan(component):
+                        component = 20
                 penalty += component
                 
         self._score = -penalty