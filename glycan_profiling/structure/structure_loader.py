4ec6091b26f88a33a07bee4b7900790c3dbe08e8
Joshua Klein <mobiusklein@gmail.com>
2018-03-19 20:08:03 -0400

    draft alternative to SQLAlchemy row storage

--- glycan_profiling/structure/structure_loader.py
+++ glycan_profiling/structure/structure_loader.py
@@ -329,3 +329,34 @@ class DecoyShiftingStubGlycopeptideStrategy(StubGlycopeptideStrategy):
 
     def _prepare_monosaccharide(self, name):
         return DecoyMonosaccharideResidue.from_iupac_lite(name)
+
+
+class GlycopeptideDatabaseRecord(object):
+    __slots__ = [
+        "id", "calculated_mass",
+        "glycopeptide_sequence",
+        "protein_id",
+        "start_position",
+        "end_position",
+        "peptide_mass",
+        "hypothesis_id"
+    ]
+
+    def __init__(self, id, calculated_mass, glycopeptide_sequence, protein_id,
+                 start_position, end_position, peptide_mass, hypothesis_id):
+        self.id = id
+        self.calculated_mass = calculated_mass
+        self.glycopeptide_sequence = glycopeptide_sequence
+        self.protein_id = protein_id
+        self.start_position = start_position
+        self.end_position = end_position
+        self.peptide_mass = peptide_mass
+        self.hypothesis_id = hypothesis_id
+
+    def __repr__(self):
+        template = (
+            "{self.__class__.__name__}(id={self.id}, calculated_mass={self.calculated_mass}, "
+            "glycopeptide_sequence={self.glycopeptide_sequence}, protein_id={self.protein_id}, "
+            "start_position={self.start_position}, end_position={self.end_position}, "
+            "peptide_mass={self.peptide_mass}, hypothesis_id={self.hypothesis_id}, ")
+        return template.format(self=self)
