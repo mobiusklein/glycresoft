ce92e9bb9cae149da3ef34a0b0b96d4389b1f4d8
Joshua Klein <mobiusklein@gmail.com>
2018-08-15 14:22:40 -0400

    Make SpectrumReference string-equivalent

--- glycan_profiling/tandem/ref.py
+++ glycan_profiling/tandem/ref.py
@@ -7,6 +7,19 @@ class SpectrumReference(object):
     def id(self):
         return self.scan_id
 
+    def __eq__(self, other):
+        try:
+            return (self.scan_id == other.scan_id) and (
+                self.precursor_information == other.precursor_information)
+        except AttributeError:
+            return self.scan_id == str(other)
+
+    def __str__(self):
+        return str(self.scan_id)
+
+    def __hash__(self):
+        return hash(self.scan_id)
+
     def __repr__(self):
         return "SpectrumReference({self.id})".format(self=self)
 
