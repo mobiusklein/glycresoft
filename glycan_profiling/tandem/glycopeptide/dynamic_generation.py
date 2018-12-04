64a0505fd228ff293fab55fd8acc2285e41ebc54
Joshua Klein <mobiusklein@gmail.com>
2018-12-03 20:34:29 -0500

    Better search techniques

--- glycan_profiling/tandem/glycopeptide/dynamic_generation.py
+++ glycan_profiling/tandem/glycopeptide/dynamic_generation.py
@@ -1,13 +1,47 @@
 import operator
+import logging
+import struct
+
+from collections import namedtuple
 
 from glycopeptidepy import GlycosylationType
 from glycopeptidepy.structure.sequence import (
     _n_glycosylation, _o_glycosylation, _gag_linker_glycosylation)
 
+
+from glycan_profiling.chromatogram_tree import Unmodified
+
+from glycan_profiling.structure.lru import LRUMapping
+from glycan_profiling.structure import FragmentCachingGlycopeptide
+
+from glycan_profiling.database import intervals
 from glycan_profiling.database.mass_collection import NeutralMassDatabase, SearchableMassCollection
 from glycan_profiling.database.builder.glycopeptide.common import limiting_combinations
 
-from .core_search import GlycanCombinationRecord
+from .core_search import GlycanCombinationRecord, GlycanFilteringPeptideMassEstimator
+from .scoring import CoverageWeightedBinomialScorer
+from ..workload import WorkloadManager
+
+
+logger = logging.Logger("glycresoft.dynamic_generation")
+
+
+_glycopeptide_key_t = namedtuple(
+    'glycopeptide_key', ('start_position', 'end_position', 'peptide_id', 'protein_id',
+                         'hypothesis_id', 'glycan_combination_id', 'flag'))
+
+
+class glycopeptide_key_t(_glycopeptide_key_t):
+    __slots__ = ()
+
+    struct_spec = struct.Struct('!LLLLLLL')
+
+    def serialize(self):
+        return self.struct_spec.pack(*self)
+
+    @classmethod
+    def parse(cls, binary):
+        return cls(*cls.struct_spec.unpack(binary))
 
 
 class GlycoformGeneratorBase(object):
@@ -16,35 +50,53 @@ class GlycoformGeneratorBase(object):
         glycan_combinations = GlycanCombinationRecord.from_hypothesis(session, hypothesis_id)
         return cls(glycan_combinations)
 
-    def __init__(self, glycan_combinations, *args, **kwargs):
+    def __init__(self, glycan_combinations, cache_size=None, *args, **kwargs):
         if not isinstance(glycan_combinations, SearchableMassCollection):
             glycan_combinations = NeutralMassDatabase(
                 list(glycan_combinations), operator.attrgetter("dehydrated_mass"))
+        if cache_size is None:
+            cache_size = 2 ** 14
+        else:
+            cache_size = int(cache_size)
         self.glycan_combinations = glycan_combinations
+        self._peptide_cache = LRUMapping(cache_size)
+        self._cache_hit = 0
+        self._cache_miss = 0
         super(GlycoformGeneratorBase, self).__init__(*args, **kwargs)
 
     def handle_glycan_combination(self, peptide_obj, peptide_record, glycan_combination,
                                   glycosylation_sites, core_type):
+        key = self._make_key(peptide_record, glycan_combination)
+        if key in self._peptide_cache:
+            self._cache_hit += 1
+            return self._peptide_cache[key]
+        self._cache_miss += 1
         glycosylation_sites_unoccupied = set(glycosylation_sites)
         for site in list(glycosylation_sites_unoccupied):
             if peptide_obj[site][1]:
                 glycosylation_sites_unoccupied.remove(site)
         site_combinations = list(limiting_combinations(glycosylation_sites_unoccupied, glycan_combination.count))
         result_set = [None for i in site_combinations]
-        key = self._make_key(peptide_record, glycan_combination)
         for i, site_set in enumerate(site_combinations):
-            glycoform = peptide_obj.clone()
+            glycoform = peptide_obj.clone(share_cache=False)
             glycoform.id = key
             glycoform.glycan = glycan_combination.composition.clone()
             for site in site_set:
                 glycoform.add_modification(site, core_type.name)
             result_set[i] = glycoform
+        result_set = Record.build(result_set)
+        self._peptide_cache[key] = result_set
         return result_set
 
-    def _make_key(self, peptide_record, glycan_combination):
-        key = (peptide_record.start_position, peptide_record.end_position,
-               peptide_record.protein_id, peptide_record.hypothesis_id,
-               glycan_combination.id)
+    def _make_key(self, peptide_record, glycan_combination, flag=0):
+        key = glycopeptide_key_t(
+            peptide_record.start_position,
+            peptide_record.end_position,
+            peptide_record.id,
+            peptide_record.protein_id,
+            peptide_record.hypothesis_id,
+            glycan_combination.id,
+            flag)
         return key
 
     def handle_n_glycan(self, peptide_obj, peptide_record, glycan_combination):
@@ -66,9 +118,14 @@ class GlycoformGeneratorBase(object):
         self.glycan_combinations = self.glycan_combinations.search_between(lower - 1, upper + 1)
         return self
 
+    def reset(self, **kwargs):
+        self._cache_hit = 0
+        self._cache_miss = 0
+        self._peptide_cache.clear()
+
 
 class PeptideGlycosylator(GlycoformGeneratorBase):
-    def __init__(self, peptide_records, glycan_combinations, *args, **kwargs):
+    def __init__(self, peptide_records, glycan_combinations, cache_size=2**16, *args, **kwargs):
         super(PeptideGlycosylator, self).__init__(glycan_combinations, *args, **kwargs)
         if not isinstance(peptide_records, SearchableMassCollection):
             peptide_records = NeutralMassDatabase(peptide_records)
@@ -104,7 +161,7 @@ class PeptideGlycosylator(GlycoformGeneratorBase):
     def generate_crossproduct(self, lower_bound=0, upper_bound=float('inf')):
         minimum_peptide_mass = max(lower_bound - self.glycan_combinations.highest_mass, 0)
         maximum_peptide_mass = max(upper_bound - self.glycan_combinations.lowest_mass, 0)
-        peptides = self.peptides.search_between(minimum_peptide_mass - 100, maximum_peptide_mass)
+        peptides = self.peptides.search_between(max(minimum_peptide_mass - 10, 0), maximum_peptide_mass)
         for peptide in peptides:
             if not peptide.has_glycosylation_sites():
                 continue
@@ -118,3 +175,215 @@ class PeptideGlycosylator(GlycoformGeneratorBase):
                 if total_mass < lower_bound or total_mass > upper_bound:
                     continue
                 yield solution
+
+    def reset(self, **kwargs):
+        super(PeptideGlycosylator, self).reset(**kwargs)
+        self.peptides.reset(**kwargs)
+
+
+class PredictiveGlycopeptideSearch(object):
+    def __init__(self, peptide_glycosylator, product_error_tolerance=2e-5, coarse_threshold=0.1,
+                 min_fragments=2, peptide_masses_per_scan=100,
+                 probing_range_for_missing_precursors=3, trust_precursor_fits=True,
+                 scorer_type=CoverageWeightedBinomialScorer):
+        self.peptide_glycosylator = peptide_glycosylator
+        self.product_error_tolerance = product_error_tolerance
+        self.coarse_threshold = coarse_threshold
+        self.min_fragments = min_fragments
+        self.peptide_mass_predictor = GlycanFilteringPeptideMassEstimator(
+            self.peptide_glycosylator.glycan_combinations,
+            product_error_tolerance)
+        self.peptide_masses_per_scan = peptide_masses_per_scan
+        self.probing_range_for_missing_precursors = probing_range_for_missing_precursors
+        self.trust_precursor_fits = trust_precursor_fits
+        self.scorer_type = scorer_type
+
+    def handle_scan_group(self, group, precursor_error_tolerance=1e-5, mass_shifts=None):
+        if mass_shifts is None:
+            mass_shifts = [Unmodified]
+        workload = WorkloadManager()
+
+        coarse_threshold = self.coarse_threshold
+        min_fragments = self.min_fragments
+        estimate_peptide_mass = self.peptide_mass_predictor.estimate_peptide_mass
+        handle_peptide_mass = self.peptide_glycosylator.handle_peptide_mass
+        peptide_masses_per_scan = self.peptide_masses_per_scan
+        for scan in group:
+            workload.add_scan(scan)
+            for mass_shift in mass_shifts:
+                seen = set()
+                intact_mass = scan.precursor_information.neutral_mass - mass_shift.mass
+                for peptide_mass in estimate_peptide_mass(scan, topn=peptide_masses_per_scan, mass_shift=mass_shift,
+                                                          threshold=coarse_threshold, min_fragments=min_fragments):
+                    for candidate in handle_peptide_mass(peptide_mass, intact_mass):
+                        if candidate.id in seen:
+                            continue
+                        seen.add(candidate.id)
+                        workload.add_scan_hit(scan, candidate, mass_shift.name)
+        return workload
+
+
+DEFAULT_THRESHOLD_CACHE_TOTAL_COUNT = 1e5
+
+
+class IterativeGlycopeptideSearch(object):
+    total_mass_getter = operator.attrgetter('total_mass')
+
+    def __init__(self, peptide_glycosylator, product_error_tolerance=2e-5, cache_size=2,
+                 scorer_type=CoverageWeightedBinomialScorer,
+                 threshold_cache_total_count=DEFAULT_THRESHOLD_CACHE_TOTAL_COUNT):
+        self.peptide_glycosylator = peptide_glycosylator
+        self.product_error_tolerance = product_error_tolerance
+        self.scorer_type = scorer_type
+        self.interval_cache = intervals.LRUIntervalSet(max_size=cache_size)
+        self.threshold_cache_total_count = threshold_cache_total_count
+
+    def _make_new_glycopeptides_for_interval(self, lower_bound, upper_bound):
+        generator = self.peptide_glycosylator.generate_crossproduct(lower_bound, upper_bound)
+        generator = NeutralMassDatabase(list(generator), self.total_mass_getter)
+        return intervals.ConcatenateMassIntervalNode(generator)
+
+    def _upkeep_memory_intervals(self, lower_bound=0, upper_bound=float('inf')):
+        """Perform routine maintainence of the interval cache, ensuring its size does not
+        exceed the upper limit
+        """
+        n = len(self.interval_cache)
+        if n > 1:
+            while (len(self.interval_cache) > 1 and
+                   self.interval_cache.total_count >
+                   self.threshold_cache_total_count):
+                logger.info("Upkeep Memory Intervals %d %d", self.interval_cache.total_count, len(self.interval_cache))
+                self.interval_cache.remove_lru_interval()
+        n = len(self.interval_cache)
+        if n == 1 and self.interval_cache.total_count > self.threshold_cache_total_count:
+            segment = self.interval_cache[0]
+            segment.constrain(lower_bound, upper_bound)
+
+    def get_glycopeptides(self, lower_bound, upper_bound):
+        self._upkeep_memory_intervals(lower_bound, upper_bound)
+        q = intervals.QueryIntervalBase(
+            (lower_bound + upper_bound) / 2., lower_bound, upper_bound)
+        match = self.interval_cache.find_interval(q)
+        if match is not None:
+            logger.debug("Nearest interval %r", match)
+            # We are completely contained in an existing interval, so just
+            # use that one.
+            if match.contains_interval(q):
+                logger.debug("Query interval %r was completely contained in %r", q, match)
+                return match.group
+            # We overlap with an extending interval, so we should populate
+            # the new one and merge them.
+            elif match.overlaps(q):
+                q2 = q.difference(match)
+                logger.debug("Query interval partially overlapped, creating disjoint interval %r", q2)
+                match = self.interval_cache.extend_interval(
+                    match,
+                    self._make_new_glycopeptides_for_interval(q2.start, q2.end)
+                )
+                return match.group
+            # We might need to insert a new interval
+            else:
+                logger.debug("Query interval %r did not overlap with %r", q, match)
+                return self._insert_interval(q.start, q.end)
+        else:
+            logger.debug("Query interval %r did not overlap with %r", q, match)
+            return self._insert_interval(q.start, q.end)
+
+    def _insert_interval(self, lower_bound, upper_bound):
+        node = self._make_new_glycopeptides_for_interval(lower_bound, upper_bound)
+        logger.debug("New Node: %r", node)
+        # We won't insert this node if it is empty.
+        if len(node.group) == 0:
+            return node.group
+        nearest_interval = self.interval_cache.find_interval(node)
+        # Should an insert be performed if the query just didn't overlap well
+        # with the database?
+        if nearest_interval is None:
+            # No nearby interval, so we should insert
+            logger.debug("No nearby interval for %r", node)
+            self.interval_cache.insert_interval(node)
+            return node.group
+        elif nearest_interval.overlaps(node):
+            logger.debug("Nearest interval %r overlaps this %r", nearest_interval, node)
+            nearest_interval = self.interval_cache.extend_interval(nearest_interval, node)
+            return nearest_interval.group
+        elif not nearest_interval.contains_interval(node):
+            logger.debug("Nearest interval %r didn't contain this %r", nearest_interval, node)
+            # Nearby interval didn't contain this interval
+            self.interval_cache.insert_interval(node)
+            return node.group
+        else:
+            # Situation unclear.
+            # Not worth inserting, so just return the group
+            logger.info("Unknown Condition Overlap %r / %r" % (node, nearest_interval))
+            return nearest_interval.group
+
+    def handle_scan_group(self, group, precursor_error_tolerance=1e-5, mass_shifts=None):
+        if mass_shifts is None:
+            mass_shifts = [Unmodified]
+
+        group = NeutralMassDatabase(group, lambda x: x.precursor_information.neutral_mass)
+        lo_mass = group.lowest_mass
+        hi_mass = group.highest_mass
+        shifts = ([m.mass for m in mass_shifts])
+
+        lo_shift = min(shifts) - 0.5
+        hi_shift = max(shifts) + 0.5
+
+        lower_bound = lo_mass + lo_shift
+        lower_bound = lower_bound - (lower_bound * precursor_error_tolerance)
+
+        upper_bound = hi_mass + hi_shift
+        upper_bound = upper_bound + (upper_bound * precursor_error_tolerance)
+
+        workload = WorkloadManager()
+        id_to_scan = {}
+        for scan in group:
+            id_to_scan[scan.id] = scan
+        logger.info("\nQuerying between %f and %f", lower_bound, upper_bound)
+        candidates = self.get_glycopeptides(lower_bound, upper_bound)
+        logger.info("Interval from %f to %f contained %d candidates", lower_bound, upper_bound, len(candidates))
+        for scan in group:
+            for mass_shift in mass_shifts:
+                intact_mass = scan.precursor_information.neutral_mass - mass_shift.mass
+                for candidate in candidates.search_mass_ppm(intact_mass, precursor_error_tolerance):
+                    workload.add_scan_hit(scan, candidate, mass_shift.name)
+        return workload
+
+
+class Record(object):
+    __slots__ = ('glycopeptide', 'id', 'total_mass')
+
+    def __init__(self, glycopeptide=None):
+        if glycopeptide is not None:
+            self.glycopeptide = str(glycopeptide)
+            self.id = glycopeptide.id
+            self.total_mass = glycopeptide.total_mass
+        else:
+            self.glycopeptide = ''
+            self.id = None
+            self.total_mass = 0
+
+    def __repr__(self):
+        return "Record(%s)" % self.glycopeptide
+
+    def __getstate__(self):
+        return (self.glycopeptide, self.id, self.total_mass)
+
+    def __setstate__(self, state):
+        self.glycopeptide, self.id, self.total_mass = state
+
+    def convert(self):
+        struct = FragmentCachingGlycopeptide(self.glycopeptide)
+        struct.id = self.id
+        return struct
+
+    @classmethod
+    def build(cls, glycopeptides):
+        return [cls(p) for p in glycopeptides]
+
+
+class Parser(object):
+    def __call__(self, record):
+        struct = record.convert()
+        return struct
