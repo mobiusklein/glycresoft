2d8f3afd13010ecea3e0e453a66993ed726c7419
Joshua Klein <mobiusklein@gmail.com>
2019-10-20 21:53:36 -0400

    Revise ScoreSet comparator

--- glycan_profiling/tandem/spectrum_match/spectrum_match.py
+++ glycan_profiling/tandem/spectrum_match/spectrum_match.py
@@ -328,26 +328,44 @@ class ScoreSet(_ScoreSet):
         return 4
 
     def __lt__(self, other):
-        if self.glycopeptide_score > other.glycopeptide_score:
+        if self.glycopeptide_score < other.glycopeptide_score:
+            return True
+        elif abs(self.glycopeptide_score - other.glycopeptide_score) > 1e-3:
             return False
-        if self.peptide_score > other.peptide_score:
+
+        if self.peptide_score < other.peptide_score:
+            return True
+        elif abs(self.peptide_score - other.peptide_score) > 1e-3:
             return False
-        if self.glycan_score > other.glycan_score:
+
+        if self.glycan_score < other.glycan_score:
+            return True
+        elif abs(self.glycan_score - other.glycan_score) > 1e-3:
             return False
-        if self.glycan_coverage > other.glycan_coverage:
-            return False
-        return True
+
+        if self.glycan_coverage < other.glycan_coverage:
+            return True
+        return False
 
     def __gt__(self, other):
-        if self.glycopeptide_score < other.glycopeptide_score:
+        if self.glycopeptide_score > other.glycopeptide_score:
+            return True
+        elif abs(self.glycopeptide_score - other.glycopeptide_score) > 1e-3:
             return False
-        if self.peptide_score < other.peptide_score:
+
+        if self.peptide_score > other.peptide_score:
+            return True
+        elif abs(self.peptide_score - other.peptide_score) > 1e-3:
             return False
-        if self.glycan_score < other.glycan_score:
+
+        if self.glycan_score > other.glycan_score:
+            return True
+        elif abs(self.glycan_score - other.glycan_score) > 1e-3:
             return False
-        if self.glycan_coverage < other.glycan_coverage:
-            return False
-        return True
+
+        if self.glycan_coverage > other.glycan_coverage:
+            return True
+        return False
 
     @classmethod
     def from_spectrum_matcher(cls, match):
@@ -364,28 +382,6 @@ class ScoreSet(_ScoreSet):
 class FDRSet(make_struct("FDRSet", ['total_q_value', 'peptide_q_value', 'glycan_q_value', 'glycopeptide_q_value'])):
     __slots__ = ()
     packer = struct.Struct("!ffff")
-
-    def __lt__(self, other):
-        if self.total_q_value > other.total_q_value:
-            return False
-        if self.glycopeptide_q_value > other.glycopeptide_q_value:
-            return False
-        if self.peptide_q_value > other.peptide_q_value:
-            return False
-        if self.glycan_q_value > other.glycan_q_value:
-            return False
-        return True
-
-    def __gt__(self, other):
-        if self.total_q_value < other.total_q_value:
-            return False
-        if self.glycopeptide_q_value < other.glycopeptide_q_value:
-            return False
-        if self.peptide_q_value < other.peptide_q_value:
-            return False
-        if self.glycan_q_value < other.glycan_q_value:
-            return False
-        return True
 
     def pack(self):
         return self.packer.pack(*self)
