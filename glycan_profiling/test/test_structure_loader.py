ff27da6eeb742f21d69a6e2686cc8df220d97593
Joshua Klein <mobiusklein@gmail.com>
2021-11-27 13:29:26 -0500

    Formally test FragmentCachingGlycopeptide

--- glycan_profiling/test/test_structure_loader.py
+++ glycan_profiling/test/test_structure_loader.py
@@ -4,10 +4,15 @@ from glycan_profiling.structure.structure_loader import (
     FragmentCachingGlycopeptide, hashable_glycan_glycopeptide_parser,
     HashableGlycanComposition, GlycanCompositionWithOffsetProxy)
 
+from glycopeptidepy.test.test_sequence import PeptideSequenceSuiteBase
+
 glycopeptide = "YPVLN(N-Glycosylation)VTMPN(Deamidation)NGKFDK{Hex:9; HexNAc:2}"
 
 
-class TestFragmentCachingGlycopeptide(unittest.TestCase):
+class TestFragmentCachingGlycopeptide(PeptideSequenceSuiteBase, unittest.TestCase):
+    def parse_sequence(self, seqstr):
+        return FragmentCachingGlycopeptide(seqstr)
+
     def test_mass(self):
         gp = FragmentCachingGlycopeptide(glycopeptide)
         self.assertAlmostEqual(gp.total_mass, 3701.5421769127897, 2)
@@ -17,6 +22,9 @@ class TestFragmentCachingGlycopeptide(unittest.TestCase):
         gc = parts[-3]
         self.assertIsInstance(
             gc, (HashableGlycanComposition, GlycanCompositionWithOffsetProxy))
+
+    test_cad_fragmentation = None
+    test_glycan_fragments_stubs = None
 
 
 if __name__ == '__main__':
