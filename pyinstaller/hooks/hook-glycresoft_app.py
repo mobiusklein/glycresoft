f667512b0a90a579327c082ad18402a3a9a5be18
Joshua Klein <mobiusklein@gmail.com>
2022-01-15 21:54:32 -0500

    Fix app hook

--- pyinstaller/hooks/hook-glycresoft_app.py
+++ pyinstaller/hooks/hook-glycresoft_app.py
@@ -1,5 +1,4 @@
 
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
-datas = list(filter(lambda x: "test_data" not in x[1] and not x[0].endswith(
-    '.c') and not x[0].endswith('.html') and not x[0].endswith('.pyx'), collect_data_files("glycresoft_app")))
+datas = list(filter(lambda x: "test_data" not in x[1], collect_data_files("glycresoft_app")))
